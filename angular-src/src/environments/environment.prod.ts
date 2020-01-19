0286a534d5c287c87120b0b169c1843fb46aecf6
Anh <vongmaianh170196@gmail.com>
2020-01-19 13:08:07 +0200

    deploying to firebase

--- angular-src/src/environments/environment.prod.ts
+++ angular-src/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    spaceId: 'onx7pfe2de1l',
+    token: 'uocQOnt1p3D2zPwk3pLhxpdTlhZg-_uvA7hxynFBm9E'
+  }
 };
